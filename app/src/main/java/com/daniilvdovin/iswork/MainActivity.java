394a082ff591c025f36d4bf608dca999b8036d44
Daniil Vdovin <45402557+DaniilVdovin@users.noreply.github.com>
2021-12-22 16:47:14 +0300

    Add item selected in navbar

--- app/src/main/java/com/daniilvdovin/iswork/MainActivity.java
+++ app/src/main/java/com/daniilvdovin/iswork/MainActivity.java
@@ -1,6 +1,7 @@
 package com.daniilvdovin.iswork;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.ImageView;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -39,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_home)
                 .setOpenableLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
